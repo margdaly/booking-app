7ac2d71ecc87b9fac1d0f08e9bc3bc8f1ef67213
margdaly <margaret.daly@gmail.com>
2023-09-20 13:34:34 -0600

    Add constant conferenceTickets, variable remainingTickets

--- main.go
+++ main.go
@@ -4,9 +4,11 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference 2023"
+	const conferenceTickets = 50
+	var remainingTickets = 50 
 
 	fmt.Println("Welcome to", conferenceName, "booking app!")
-	fmt.Println("Get your tickets now!")
-
-	fmt.Println(conferenceName)
+	fmt.Println("Total tickets available *",conferenceTickets,"*")
+	fmt.Println("And only *",remainingTickets,"* tickets left!")
+	fmt.Println("Get Your Tickets Now!")
 }
