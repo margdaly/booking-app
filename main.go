5f11d98404d506c0e06eb2ec056f6a2ad67e72a9
margdaly <margaret.daly@gmail.com>
2023-09-20 13:04:55 -0600

    Add fmt package & conferenceName

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var conferenceName = "Go Conference 2023"
+
+	fmt.Println("Welcome to", conferenceName, "booking app!")
+	fmt.Println("Get your tickets now!")
+
+	fmt.Println(conferenceName)
+}
