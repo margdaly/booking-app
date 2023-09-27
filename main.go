5df4e00aa54ba33ce5200237c3d99fe5dd4046b2
margdaly <margaret.daly@gmail.com>
2023-09-27 14:41:43 -0600

    feat: Add user input for first/last name & email

--- main.go
+++ main.go
@@ -3,12 +3,35 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference 2023"
+	conferenceName := "Go Conference 2023" 
+	// alternative syntax for assigning variables
 	const conferenceTickets = 50
-	var remainingTickets = 50 
+	var remainingTickets uint = 50 
+	// uint is an unsigned integer, cannot be negative
 
-	fmt.Println("Welcome to", conferenceName, "booking app!")
-	fmt.Println("Total tickets available *",conferenceTickets,"*")
-	fmt.Println("And only *",remainingTickets,"* tickets left!")
-	fmt.Println("Get Your Tickets Now!")
+	fmt.Printf("Welcome to %v booking app!\n", conferenceName)
+	fmt.Printf("Total tickets available ** %v ** and only ~ %v ~ tickets left!\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get Your Tickets Now!")	
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+	// ask user for their name
+
+	fmt.Println("Please enter your first name:")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Please enter your last name:")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Please enter your email address:")
+	fmt.Scan(&email)
+
+	fmt.Println("How many tickets would you like to book?")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets with us!\n", firstName, lastName,userTickets)
+	fmt.Printf("An email confirmation has been sent to %v.\n", email)
+
 }
